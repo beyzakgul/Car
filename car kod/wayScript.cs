257acfeeb4dad06e4fe72f0d8edc4e6b8888d8c3
beyzakgul <101328948+beyzakgul@users.noreply.github.com>
2023-03-09 21:52:31 +0300

    Car Game
    Car Game kodları

---
+++ car kod/wayScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wayScript : MonoBehaviour
+{
+    public GameObject way;
+    bool yol_yapýldý = false;
+
+    void OnTriggerEnter2D(Collider2D temas)
+    {
+        if (temas.gameObject.tag=="mainCar"&& yol_yapýldý==false)
+        {
+            Vector3 spawn_location = new Vector3(transform.position.x, transform.position.y + 10, 0);
+            Instantiate(way, spawn_location, Quaternion.identity);
+            yol_yapýldý=true;
+            scorManager.skor += 10;
+            Destroy(this.gameObject,6f);
+        }
+    }
+}
